700c69655a4f218b15d538ca7a214c8195f51688
Ezeofor PraiseGod <senitenttechnology@gmail.com>
2025-10-10 13:05:20 +0100

    implement daily news summary

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,8 +1,8 @@
 import { inngest } from '@/lib/ingest/client';
-import { sendSignUpEmail } from '@/lib/ingest/funtions';
+import { sendDailyNewsSummary, sendSignUpEmail } from '@/lib/ingest/funtions';
 import { serve } from 'inngest/next';
 
 export const { GET, POST, PUT } = serve({
 	client: inngest,
-	functions: [sendSignUpEmail],
+	functions: [sendSignUpEmail, sendDailyNewsSummary],
 });
